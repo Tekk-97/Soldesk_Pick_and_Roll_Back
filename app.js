4ff2ac03a1203c5da798199cfc31692086dafb84
w-ryan-jung <w.ryan.jung@gmail.com>
2023-12-11 22:42:37 +0900

    start up

---
+++ app.js
@@ -0,0 +1,28 @@
+import express from "express";
+import "express-async-errors";
+import cors from "cors";
+import morgan from "morgan";
+import helmet from "helmet";
+import tweetsRouter from "./router/tweets.js";
+import authRouter from "./router/auth.js";
+import healthzRouter from "./router/healthz.js";
+import bodyParser from "body-parser";
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(helmet());
+app.use(morgan("combined"));
+
+app.use("/tweets", tweetsRouter);
+app.use("/auth", authRouter);
+app.use("/healthz", healthzRouter);
+app.use((req, res, next) => {
+  res.sendStatus(404);
+});
+
+app.use((error, req, res, next) => {
+  console.error(error);
+  res.sendStatus(500);
+});
+app.listen(80);
